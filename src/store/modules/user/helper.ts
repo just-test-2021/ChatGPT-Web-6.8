92b6e3e4b01dc84dca554864e13d3c38965ea89f
4:30 <79741736+just-test-2021@users.noreply.github.com>
2023-06-08 10:07:18 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://avatars.githubusercontent.com/u/100025239?s=400&u=2fd3a7f25544efc7c3bf9e7e334e34f98caa5c65&v=4',
-      name: '大聪明',
-      description: '大聪明复活版',
+      name: 'BigSmart',
+      description: 'BigSmart Revived',
     },
   }
 }
